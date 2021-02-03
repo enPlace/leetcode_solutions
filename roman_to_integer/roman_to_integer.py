c08cb51b245b562a110cfccf4923fb8e030d15d1
enPlace <n.place@protonmail.com>
2021-02-03 14:30:02 -0300

    updating roman_to_integer.py

--- roman_to_integer/roman_to_integer.py
+++ roman_to_integer/roman_to_integer.py
@@ -1,40 +1,12 @@
 """Given a roman numeral, convert it to an integer."""
 
-# One way to do this is to create special rules for instances where 
-# subtraction is used after adding all of the individual numerals of 
-# the string. 
-def romanToInt(s):
-        numerals = {"I": 1, "V": 5, "X":10, "L":50, "C":100, "D": 500, "M": 1000}
-        answer = 0
-        if 1<= len(s) <=15:
-            for i in s:
-                answer += numerals[i]
-            if "IV"  in s: 
-                answer -=2
-            if "IX"  in s: 
-                answer -=2
-            if "XL" in s: 
-                answer-=20
-            if "XC" in s: 
-                answer-=20
-            if "CD" in s: 
-                answer-=200
-            if "CM" in s: 
-                answer-=200
-        return answer
 
-print(romanToInt("IV"))
-print(romanToInt("III"))
-print(romanToInt("IX"))
-print(romanToInt("LVIII"))
-print(romanToInt("MCMXCIV"))
-print(romanToInt("IVIV"))
 
-#A simpler, cleaner way to do it would be to add another dictionary with those special 
-#2-letter numerals, with a negative number for each value. You could iterate 
-#through the numeral and subtract if it finds any instances of the 2-letter numeral. This is
+#A simple way to do this would be to add a dictionary with those special 
+#2-letter numerals (IV; IX; XL, etc), with a negative number for each value. We can iterate 
+#through the numeral and subtract if our loop finds any instances of each 2-letter numeral. This is
 # assuming that the roman numeral is written correctly. If it is, any 2-letter numeral would
-# only occur once. 
+# only occur once.
 def romanToInt(s):
         numerals = {"I": 1, "V": 5, "X":10, "L":50, "C":100, "D": 500, "M": 1000}
         two_letter = {"IV": -2, "IX": -2, "XL": -20, "XC": -20, "CD": -200, "CM": -200}
@@ -55,6 +27,6 @@ print(romanToInt("III"))
 print(romanToInt("IX"))
 print(romanToInt("LVIII"))
 print(romanToInt("MCMXCIV"))
-print(romanToInt("IVIV"))
+
 
 
